2ff12e9f92d3aab988dcc2abc412b189f740c08d
Júnior Silva <junior.silva@alu.ufc.br>
2024-10-03 17:21:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import QRCode from "react-qr-code";
+import QRCodeLink from "qrcode";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+    const [text, setText] = useState("");
+    const [qrCode, setQrCode] = useState("");
+    // const [darkMode, setDarkMode] = useState(false);
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+        handleGenerate(e.target.value);
+    };
+
+    const handleGenerate = (link_url) => {
+        QRCodeLink.toDataURL(
+            link_url,
+            {
+                width: 600,
+                margin: 3,
+            },
+            (err, url) => {
+                setQrCode(url);
+            }
+        );
+    };
+
+    return (
+        <div className="container">
+            <h1>QR Code Generator</h1>
+            <input
+                type="text"
+                className="input"
+                placeholder="Digite aqui seu link ou texto..."
+                onChange={handleChange}
+            />
+            <div className="qrcode">
+                <QRCode value={text} />
+            </div>
+            <a href={qrCode} download={"qrcode.png"} className="botao_salvar">
+                Download
+            </a>
+        </div>
+    );
+}
+
+export default App;
